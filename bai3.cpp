ad44574c5c94fa71ade82c308b194913d943d01a
unknown <nhuha258@gmail.com>
2022-04-04 17:21:07 +0700

    nopBT

--- bai3.cpp
+++ bai3.cpp
@@ -4,48 +4,40 @@ class DaThuc3{
 	private:
 	int a, b, c, d;
 	public:
-	/*DaThuc3(int, int, int, int);
-	~DaThuc3();*/
-	void nhap();
-	void in();
-	int geta(){
-		return this->a;
+	DaThuc3(int bac3 = 0, int bac2 = 0, int bac1 = 0, int bac0 = 0){
+		this ->a = bac3;
+		this ->b = bac2;
+		this ->c = bac1;
+		this ->d = bac0;
 	}
-	int getb(){
-		return this->b;
-	}
-	int getc(){
-		return this->c;
-	}
-	int getd(){
-		return this->d;
-	}
+	~DaThuc3(){}	
+	friend istream& operator >> (istream& in, DaThuc3& a);
+	friend ostream& operator << (ostream& out, DaThuc3 a);
+	DaThuc3 operator + (DaThuc3 a);
 };
-void DaThuc3::nhap(){
-	cout << "\nBac 3 : ";cin >> this->a;
-	cout << "Bac 2 : ";cin >> this->b;
-	cout << "Bac 1 : ";cin >> this->c;
-	cout << "Bac 0 : ";cin >> this->d;
-};
-/*DaThuc3::DaThuc3(int bac3, int bac2, int bac1, int bac0){
-	cout << "Ham khoi tao co tham so" << endl;
-	this->a = bac3;
-	this->b = bac2;
-	this->c = bac1;
-	this->d = bac0;
-	cout << this->a << "x^3 + " << this->b << "x^2 + " << this->c << "x + " << this->d << endl;
+istream& operator >> (istream& in, DaThuc3& a){
+	cout << "a = ";in >> a.a;
+	cout << "b = ";in >> a.b;
+	cout << "c = ";in >> a.c;
+	cout << "d = ";in >> a.d;
+	return in;
 }
-DaThuc3::~DaThuc3(){
-	cout << "Da thuc duoc huy "<< endl;
-}*/
+ostream& operator << (ostream& out, DaThuc3 a){
+	out << a.a << "x^3 + " << a.b << "x^2 + " << a.c << "x + " << a.d << endl;
+	return out;
+}
+DaThuc3 DaThuc3::operator + (DaThuc3 a){
+	DaThuc3 tong;
+	tong.a = this ->a + a.a;
+	tong.b = this ->b + a.b;
+	tong.c = this ->c + a.c;
+	tong.d = this ->d + a.d;
+	return tong;
+}
 int main(){
-	DaThuc3 t[2];
-	for(int i = 0;i < 2; i++){
-		t[i].nhap();
-	}
-	cout << t[0].geta() + t[1].geta() << "x^3 + ";
-	cout << t[0].getb() + t[1].getb() << "x^2 + ";
-	cout << t[0].getc() + t[1].getc() << "x + " ;
-	cout << t[0].getd() + t[1].getd() << endl;
+	DaThuc3 x, y;
+	cin >> x >> y;
+	DaThuc3 tong = x + y;
+	cout << tong;
 	return 0;
 }
